0a12168f5482b5dc7d89113c1905dc843171ce4e
Achu Mukundan <achuthanmukundan00@gmail.com>
2019-01-22 23:25:42 -0500

    Refine landing page styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SignInComponent } from './pages/login/sign-in/sign-in.component';
-import { SignUpComponent } from './pages/login/sign-up/sign-up.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -14,14 +12,14 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { AuthGuard } from './core/auth.guard';
 import { AuthService } from './core/auth.service';
+import { EmptyNavbarComponent } from './empty-navbar/empty-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignInComponent,
-    SignUpComponent,
-    routingComponents
+    routingComponents,
+    EmptyNavbarComponent
   ],
   providers: [AuthGuard, AuthService],
   imports: [
