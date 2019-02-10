0521999b5f6d31cd5f41a3437c154467849c9096
Achu Mukundan <achuthanmukundan00@gmail.com>
2019-02-10 09:46:58 -0500

    Fix bug
    - Project page reinitializes but subscription does not fetch firebase documents
    - Wrap  subscription in promise to fix

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -12,19 +12,36 @@ import { Project } from '../../models/project';
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[];
+  editState = false;
+  projectToEdit: Project;
 
   constructor(private projectService: ProjectService ) {
-
   }
 
-  ngOnInit() {
-    this.projectService.getProjects().subscribe(projects => {
-      this.projects = projects;
-    });
+  async ngOnInit() {
+    this.projects = await this.projectService.getProjects();
   }
 
-  onSubmit(project) {
+  onSubmit(event, project: Project) {
+    this.clearState();
     this.projectService.deleteProject(project);
   }
+
+  editProject(event, project: Project) {
+    this.editState = true;
+    this.projectToEdit = project;
+  }
+
+  clearState() {
+    this.editState = false;
+    this.projectToEdit = null;
+  }
+
+  updateProject(project: Project) {
+    this.projectService.updateProject(project);
+    this.clearState();
+  }
+
+
 
 }
