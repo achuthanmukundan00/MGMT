4b370013def0c5069f38c1cda98399df7d0c3e0d
Achu Mukundan <achumukundan@localhost.localdomain>
2019-02-21 23:52:58 -0500

    Implement user search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './core/authentication/auth.service';
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'mgmtProject';
-  items: Observable<any[]>;
-  constructor(db: AngularFirestore, public auth: AuthService) {
-    this.items = db.collection('items').valueChanges();
+  constructor(public auth: AuthService, ) {
   }
 }
