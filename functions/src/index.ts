77618ab410d75bb95388526cb0db1b0b2939096e
Achuthan Mukundan <achuthanmukundan00@gmail.com>
2019-08-10 10:24:16 -0400

    Version 1.0.0 Complete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,10 @@
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
